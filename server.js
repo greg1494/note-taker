0744c7876bab11e6a30b21971a4a480a05695d93
Greg Christiansen <gregchristiansen14@gmail.com>
2021-12-17 16:47:34 -0600

    working on the server for the GET and POST requests

--- server.js
+++ server.js
@@ -1,12 +1,61 @@
 const express = require('express');
-const { notes } = require('./data/notes');
+const path = require('path');
+const { title } = require('process');
+const uuid = require('./helpers/uuid');
+
+
+const PORT = 3001;
 
 const app = express();
 
-app.get('/data/notes', (req, res) => {
-    res.send('Hello World');
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'));
 });
 
-app.listen(3001, () => {
-    console.log(`Api server now on port 3001!`);
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/notes.html'));
 });
+app.get('/api/notes', (req, res) => {
+    // Inform the client
+    res.json(`${req.method} request received to get notes`);
+  
+    // Log our request to the terminal
+    console.info(`${req.method} request received to notes`);
+});
+ 
+app.post('/api/notes', (req, res) => {
+    // Inform the client that their POST request was received
+    res.json(`${req.method} request received to notes`);
+  
+    // Destructuring assignment for the items in req.body
+  const { title, text } = req.body;
+
+  // If all the required properties are present
+  if (title, text) {
+    // Variable for the object we will save
+    const newNote = {
+      title,
+      text,
+      review_id: uuid(),
+    };
+
+    const response = {
+      status: 'success',
+      body: newNote,
+    };
+
+    console.log(response);
+    res.json(response);
+  } else {
+    res.json('Error in posting note');
+  }
+});
+
+app.listen(PORT, () =>
+    console.log(`app listening at http://localhost:${PORT}`)
+);
