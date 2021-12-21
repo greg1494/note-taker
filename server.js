a24558305e373a574f4c97058a4f89aa16bcc133
Greg Christiansen <gregchristiansen14@gmail.com>
2021-12-20 18:17:00 -0600

    removed some code that was not being used

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
-// Helper method for generating unique ids
-const uuid = require('./helpers/uuid');
 const PORT = process.env.PORT || 3001
 const app = express();
 
